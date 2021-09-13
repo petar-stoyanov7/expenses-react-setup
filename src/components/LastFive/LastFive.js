dde76b57d9474f11c701ceb9497b97de59abe0e9
Petar Stoyanov <petar.tech@condor-gaming.com>
2021-09-13 14:06:59 +0300

    LastFive;
    + Finished getting data for last five;
    + Basic styling;

--- src/components/LastFive/LastFive.js
+++ src/components/LastFive/LastFive.js
@@ -8,6 +8,7 @@ import React, {
 import './LastFive.scss';
 import AuthContext from "../../Store/auth-context";
 import axios from "axios";
+import ExpenseTable from "../UI/ExpenseTable";
 // import ExpenseTable from "../UI/ExpenseTable";
 
 const dummyData = [
@@ -51,7 +52,6 @@ const LastFive = (props) => {
 
     useEffect(() => {
         const ajaxCfg = ctx.ajaxConfig;
-        console.log('ajax',ajaxCfg);
         let data = {
             hash: ctx.ajaxConfig.hash
         }
@@ -66,17 +66,59 @@ const LastFive = (props) => {
                 data['userId'] = ctx.userDetails.user.id;
                 break;
         }
-        axios.post(`${ajaxCfg.server}${ajaxCfg.getLastFive}`, data)
-            .then((response) => {
-                console.log('last five');
-                console.log(response);
-            });
-    }, [ctx]);
+        if (undefined !== data['userId'] || undefined !== data['carId']) {
+            axios.post(`${ajaxCfg.server}${ajaxCfg.getLastFive}`, data)
+                .then((response) => {
+                    const result = response.data
+                    if (result.success) {
+                        console.log('exp_raw');
+                        console.log(result.data);
+                        const formattedData = result.data.map((expense) => {
+                            const exp = {
+                                id: expense.ID,
+                                carName: `${expense.car_brand} ${expense.car_model}`,
+                                date: expense.Date,
+                                type: expense.expense_name,
+                                cid: expense.CID,
+                                fuelId: expense.Fuel_ID,
+                                liters: expense.Liters,
+                                insuranceId: expense.Insurance_ID,
+                                mileage: expense.Mileage,
+                                price: expense.Price,
+                                notes: expense.Notes
+                            }
+                            switch (expense.Expense_ID) {
+                                case '1':
+                                    exp['expenseDetail'] = expense.fuel_name;
+                                    break;
+                                case '2':
+                                    exp['expenseDetail'] = expense.insurance_name;
+                                    break;
+                                case '3':
+                                case '4':
+                                case '999':
+                                default:
+                                    exp['expenseDetail'] = '';
+                                    break;
+                            }
+                            return exp;
+                        })
+                        setLastFive(formattedData);
+                    } else {
+                        setLastFive(dummyData);
+                    }
+                });
+        } else {
+            setLastFive(dummyData);
+        }
+    }, [ctx.userDetails.isLogged]);
 
     return (
         <Fragment>
             <h3>Last five:</h3>
-            {/*<ExpenseTable expenses={lastFive} />*/}
+            <ExpenseTable
+                expenses={lastFive}
+            />
         </Fragment>
     );
 }
--- src/components/UI/ExpenseTable.js
+++ src/components/UI/ExpenseTable.js
@@ -3,55 +3,85 @@ import './ExpenseTable.scss';
 
 const ExpenseTable = (props) => {
     let expenses;
+    console.log('exp', props.expenses);
+
 
     if (null === props.expenses || props.expenses.length === 0) {
         console.log('no expenses');
         expenses = '<td>No expenses recorded</td>'
     } else {
         expenses = props.expenses.map((expense) => {
-            let expenseName;
-            switch (expense.Expense_ID) {
-                case 1:
-                    expenseName = 'Fuel';
-                    break;
-                case 2:
-                    expenseName = expense.insuranceName;
-                    break;
-                default:
-                    expense = '';
-                    break;
-            }
-
             return (
-                <tr key={expense.id}>
-                    <td>{expense.mileage}</td>
-                    <td>{expense.date}</td>
-                    <td>{expense.carName}</td>
-                    <td>{expenseName}</td>
-                    <td>{expense.fuelType}</td>
-                    <td>{expense.liters}</td>
-                    <td>{expense.price}</td>
-                    <td>{expense.notes}</td>
-                    <td>---</td>
+                <tr
+                    key={expense.id}
+                    onClick={null == props.clickAction ? undefined : () => {
+                        props.clickAction(expense.id);
+                    }}
+                >
+                    <td className="expenses-list__mileage">
+                        {expense.mileage}
+                    </td>
+                    <td className="expenses-list__date">
+                        {expense.date}
+                    </td>
+                    <td className="expenses-list__car">
+                        {expense.carName}
+                    </td>
+                    <td className="expenses-list__type">
+                        {expense.type}
+                    </td>
+                    <td className="expenses-list__detail">
+                        {expense.expenseDetail}
+                    </td>
+                    <td className="expenses-list__liters">
+                        {expense.liters}
+                    </td>
+                    <td className="expenses-list__price">
+                        {expense.price}
+                    </td>
+                    <td className="expenses-list__notes">
+                        {expense.notes}
+                    </td>
                 </tr>
             )
         });
     }
 
     return (
-        <table className="expenses-list">
-            <thead>
-            <tr>
-                <th>Mileage</th>
-                <th>Date</th>
-                <th>Car</th>
-                <th>Type</th>
-                <th>Fuel Type</th>
-                <th>Liters</th>
-                <th>Value</th>
-                <th>Notes</th>
-                <th>Actions</th>
-            </tr>
+        <table
+            className={`expenses-list
+                ${props.isDetailed} ? ' detailed' : ''}
+                ${props.isSmall} ? ' small' : ''}
+            `}
+            cellSpacing='0'
+        >
+            <thead className='expenses-list__header'>
+                <tr>
+                    <th className="expenses-list__mileage">
+                        Mileage
+                    </th>
+                    <th className="expenses-list__date">
+                        Date
+                    </th>
+                    <th className="expenses-list__car">
+                        Car
+                    </th>
+                    <th className="expenses-list__type">
+                        Type
+                    </th>
+                    <th className="expenses-list__detail">
+                        Detail
+                    </th>
+                    <th className="expenses-list__liters">
+                        Liters
+                    </th>
+                    <th className="expenses-list__price">
+                        Value
+                    </th>
+                    <th className="expenses-list__notes">
+                        Notes
+                    </th>
+                </tr>
             </thead>
             <tbody>
                 {expenses}
